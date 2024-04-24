adfa380eb31fc7a582557e0131ca55ab4c1bdfb3
Anish Mainali <130769914+anishmendes@users.noreply.github.com>
2024-04-24 16:13:22 +0545

    navbar changed

--- src/components/Launch/Hero.jsx
+++ src/components/Launch/Hero.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Hero = () => {
     return (
         <div id='hero' className='flex md:flex-row flex-col h-max md:h-[120vh] min-h-screen justify-between px-[10%] items-center'>
-         
+          
         </div>
     )
 }
