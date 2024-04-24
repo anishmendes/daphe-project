e37b83224e84f117b3cc4bf158609eac6689c717
Anish Mainali <130769914+anishmendes@users.noreply.github.com>
2024-04-24 11:48:51 +0545

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Launch from "./pages/Launch";
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+AOS.init();
+
+
+const App = () => {
+  return (
+    <Launch />
+  )
+}
+
+export default App
