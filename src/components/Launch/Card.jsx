9886f24985713559707447a11632cbf5559e67fa
Anish Mainali <130769914+anishmendes@users.noreply.github.com>
2024-04-25 11:45:43 +0545

    auto slide done

--- src/components/Launch/Card.jsx
+++ src/components/Launch/Card.jsx
@@ -4,47 +4,47 @@ import 'swiper/swiper-bundle.css'; // Import Swiper's CSS
 import demo from '../../assets/demoImage.png'
 
 
-import {   Autoplay, EffectCoverflow, Pagination } from 'swiper/modules';
+import {   Autoplay,  Pagination } from 'swiper/modules';
 
 
 const Card = () => {
   return (
 
-    <div className="flex justify-center items-center h-screen">
+    <div className="flex justify-center items-center h-screen w-screen">
 
-    <div className="relative w-3/4 h-3/4 ">
+    <div className="relative w-3/4  h-3/4 ">
       <Swiper
          effect={'coverflow'}
          
          grabCursor={true}
          centeredSlides={true}
          autoplay={{
-            delay: 1500,
+            delay: 1700,
             disableOnInteraction: false,
           }}
-         slidesPerView={'auto'}
-         coverflowEffect={{
-           rotate: 50,
-           stretch: 0,
-           depth: 100,
-           modifier: 1,
-           slideShadows: true,
-         }}
+         slidesPerView={'2'}
+        //  coverflowEffect={{
+        //    rotate: 50,
+        //    stretch: 0,
+        //    depth: 100,
+        //    modifier: 1,
+        //    slideShadows: true,
+        //  }}
          pagination={true}
-         modules={[Autoplay,  EffectCoverflow, Pagination]}
+         modules={[Autoplay, Pagination]}
         className="w-full h-full"
         >
         <SwiperSlide>
-          <img src={demo} className="block w-full h-full" alt="card1" />
+          <img src={demo} className="object-cover  w-100% h-10%" alt="card1" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={demo} className="block w-full h-full" alt="card2" />
+          <img src={demo} className="object-cover  w-100% h-10%" alt="card2" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={demo}className="block w-full h-full" alt="card3" />
+          <img src={demo}className="object-cover w-100% h-10%" alt="card3" />
         </SwiperSlide>
         <SwiperSlide>
-          <img src={demo} className="block w-full h-full" alt="card4" />
+          <img src={demo} className="object-cover  w-100% h-10%" alt="card4" />
         </SwiperSlide>
       </Swiper>
     </div>
