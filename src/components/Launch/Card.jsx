7755178282e370f0d056532a037c3cbda6877282
Anish Mainali <130769914+anishmendes@users.noreply.github.com>
2024-04-25 11:30:36 +0545

    card component added

--- src/components/Launch/Card.jsx
+++ src/components/Launch/Card.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css'; // Import Swiper's CSS
+import demo from '../../assets/demoImage.png'
+
+
+import {   Autoplay, EffectFade, Pagination } from 'swiper/modules';
+
 
 const Card = () => {
   return (
-    <div>
-      My Card 
-    </div>
-  )
-}
 
-export default Card
+    <div className="flex justify-center items-center h-screen">
+
+    <div className="relative w-3/4 h-3/4 ">
+      <Swiper
+         effect={'fade'}
+         
+         grabCursor={true}
+         centeredSlides={true}
+         autoplay={{
+            delay: 1500,
+            disableOnInteraction: false,
+          }}
+         slidesPerView={'auto'}
+         coverflowEffect={{
+           rotate: 50,
+           stretch: 0,
+           depth: 100,
+           modifier: 1,
+           slideShadows: true,
+         }}
+         pagination={true}
+         modules={[Autoplay, EffectFade, Pagination]}
+        className="w-full h-full"
+        >
+        <SwiperSlide>
+          <img src={demo} className="block w-full h-full" alt="card1" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={demo} className="block w-full h-full" alt="card2" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={demo}className="block w-full h-full" alt="card3" />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={demo} className="block w-full h-full" alt="card4" />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+          </div>
+  );
+};
+
+export default Card;
