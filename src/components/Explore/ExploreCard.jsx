7ff7309a64798f0f196c2046480d4095e17ed1a7
Anish Mainali <130769914+anishmendes@users.noreply.github.com>
2024-04-26 12:46:45 +0545

    ...

--- src/components/Explore/ExploreCard.jsx
+++ src/components/Explore/ExploreCard.jsx
@@ -107,7 +107,7 @@ const ExploreCard = () => {
           {hoveredIndex === index && (
             <>
              <h4 className="text-xl font-bold">{val.name}</h4>
-                            <p className="text-sm m-[40px]">{val.desc}</p>
+                            <p className="text-sm  rounded-md  m-[20px]">{val.desc}</p>
              
                 <button  className='outline-btn bg-blue-600 text-white border border-teal-400 hover:bg-blue-700 hover:text-white transition duration-500 ease-in-out'>See More </button>
             
