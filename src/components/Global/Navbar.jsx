d28e8eb31d7963ebbbe475709d6da5f6026957bb
Anish Mainali <130769914+anishmendes@users.noreply.github.com>
2024-04-27 17:47:34 +0530

    footer and navbar changes

--- src/components/Global/Navbar.jsx
+++ src/components/Global/Navbar.jsx
@@ -1,31 +1,53 @@
 import React from 'react'
-import logo from '../../assets/logo.png';
-import { Link } from 'react-router-dom';
+import ContactDialog from './ContactDialog/ContactDialog'
+import { Link } from 'react-router-dom'
 
-const Navbar = () => {
+
+const Footer = () => {
+
+    const footerRouteTo = () => {
+        window.scrollTo({
+            top: 0,
+        });
+    };
+
     return (
-        <>
-            <div className='fixed top-[26px] flex ml-[2%] z-50 w-20 items-center justify-center'>
-            <Link to="/">
-
-                <img src={logo} className='w-full h-full' alt="" />
-            </Link>
+        <div data-aos='fade-up' data-aos-duration='700' data-aos-once='true' className='min-h-[70vh] h-max flex px-[10%] items-start justify-between relative flex-col'>
+            <div className='w-full mt-24 md:mt-20 flex justify-between flex-col md:flex-row gap-16 md:gap-0'>
+                <div className='flex md:gap-32 gap-[20%] md:justify-start'>
+                    <div className='flex flex-col gap-4'>
+                        <div className=' text-neutral-400 font-light tracking-tight md:text-[36px]/[95%] text-[28px]/[95%]'>General Links</div>
+                        <Link onClick={footerRouteTo} to='/' className='tracking-tight md:text-[36px]/[95%] text-[28px]/[95%] hover:opacity-80 transition-all'>Launch</Link>
+                        <Link onClick={footerRouteTo} to='/research' className='tracking-tight md:text-[36px]/[95%] text-[28px]/[95%] hover:opacity-80 transition-all'>Research</Link>
+                        <Link onClick={footerRouteTo} to='/team' className='tracking-tight md:text-[36px]/[95%] text-[28px]/[95%] hover:opacity-80 transition-all'>Our Team</Link>
+                        <Link onClick={footerRouteTo} to='/explore' className='tracking-tight md:text-[36px]/[95%] text-[28px]/[95%] hover:opacity-80 transition-all'>Explore</Link>
+                    </div>
+                    <div className='flex flex-col gap-4'>
+                        <div className=' text-neutral-400 font-light tracking-tight md:text-[36px]/[95%] text-[28px]/[95%]'>Socials</div>
+                        <a href='#' className='tracking-tight md:text-[36px]/[95%] text-[28px]/[95%] hover:opacity-80 transition-all'>Instagram</a>
+                        <a href='https://m.facebook.com/100069567636985/' target='_blank' className='tracking-tight md:text-[36px]/[95%] text-[28px]/[95%] hover:opacity-80 transition-all'>Facebook</a>
+                        <a href='#' className='tracking-tight md:text-[36px]/[95%] text-[28px]/[95%] hover:opacity-80 transition-all'>LinkedIn</a>
+                    </div>
+                </div>
+                <div className='flex flex-col md:w-[27.5%] items-start gap-6'>
+                    <div className='tracking-tighter text-neutral-800 font-medium text-[16px]/[120%]'>Have questions?</div>
+                    <ContactDialog title='Contact Us' classList='w-full text-center bg-sky-500 tracking-tighter text-white p-4 rounded-xl font-medium text-[16px]/[120%] hover:opacity-80' />
+                    <div className='flex flex-col gap-2'>
+                        <div className='tracking-tighter text-neutral-800 font-medium text-[16px]/[120%]'>Stay Updated</div>
+                        <div className='tracking-tighter text-neutral-400 font-medium text-[16px]/[120%]'>Drop your email to know what's coming</div>
+                    </div>
+                    <form action="" className='relative flex w-full items-center'>
+                        <input type='email' className='w-full text-start bg-neutral-100 border-neutral-300 border tracking-tighter text-black outline-none focus:ring ring-sky-200 p-4 rounded-xl font-medium text-[16px]/[120%]' />
+                        <input type="submit" value='>' className='absolute right-0 flex pr-4 text-neutral-600 text-lg hover:text-2xl transition-all duration-300 cursor-pointer' />
+                    </form>
+                </div>
             </div>
-            <div className='flex w-full fixed top-[32px] justify-end h-max z-50 items-center'>
-    <div className='mr-4 text-center flex py-2 px-4 bg-none rounded-lg gap-8 uppercase items-center justify-center'>
-
-
-        <Link to='/' className='navLink activeNavLink' >Launch</Link>
-        <a className='navLink' >Research</a>
-        <a className='navLink' >Team</a>
-        <Link to="/explore" className="navLink">Explore</Link>
-    </div>
-</div>
-
-                {/* <button className='flex mr-[40px] py-2 px-4 bg-sky-600 text-center tracking-tighter text-white rounded-xl text-[16px]/[120%] hover:opacity-80'>Reach Us</button> */}
-
-        </>
+            <div className='flex flex-col pb-10 pt-16 md:pt-0'>
+                <div className='tracking-tight text-neutral-400 font-light text-[16px]/[120%]'>@2024 Danphe Rocketry</div>
+                <div className='tracking-tight text-neutral-800 font-regular text-[16px]/[120%]'>Designed and Maintained by <a className='underline' href="https://project0rbit.com">Antarikshya</a></div>
+            </div>
+        </div>
     )
 }
 
-export default Navbar
+export default Footer
